3b727269b147afa6405bf9c101005befa19018e1
Nishit Bansal <nishit@Macbook-Air.local>
2023-03-18 12:09:42 +0530

    Re-structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,102 @@
-import './App.scss';
-import Navbar from './components/Navbar';
-import Simulator from './components/Simulator';
+import "./App.scss";
+import Navbar from "./components/Navbar";
+import Simulator from "./components/Simulator";
+import React, { useCallback, useRef, useState } from "react";
+
+let numRows = 20,
+  numCols = 20;
+const genGrid = () => {
+  let rows = [];
+  for (let i = 0; i < numRows; i++) {
+    // rows.push(Array.from(Array(numCols), () => 0));
+    rows.push(Array.from(Array(numCols), () => Math.floor(Math.random() * 2)));
+  }
+  return rows;
+};
+const positions = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0],
+];
 
 function App() {
+  const [grid, setGrid] = useState(() => genGrid());
+  const [running, setRunning] = useState(false);
+  //   console.log(grid);
+
+  // ...
+  const runningRef = useRef(running);
+  runningRef.current = running;
+
+  const runSimulation = useCallback((grid) => {
+    if (!runningRef.current) {
+      return;
+    }
+
+    let gridCopy = [...grid];
+    for (let i = 0; i < numRows; i++) {
+      for (let j = 0; j < numCols; j++) {
+        let neighbors = 0;
+
+        positions.forEach(([x, y]) => {
+          const newI = i + x;
+          const newJ = j + y;
+
+          if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
+            neighbors += grid[newI][newJ];
+          }
+        });
+
+        if (neighbors < 2 || neighbors > 3) {
+          gridCopy[i][j] = 0;
+        } else if (grid[i][j] === 0 && neighbors === 3) {
+          gridCopy[i][j] = 1;
+        }
+      }
+    }
+
+    setGrid(gridCopy);
+  }, []);
+
+  const handleReset = ()=>{
+    let copy = genGrid();
+    console.table(copy);
+
+    setGrid(copy);
+  }
   return (
     <div className="app">
-      <Navbar/>
-      <Simulator/>
+      <Navbar />
+      <div className="simulator">
+        <Simulator
+          rows={numRows}
+          cols={numCols}
+          grid={grid}
+          setGrid={setGrid}
+        />
+        <div className="buttons">
+          <button className="button" onClick={()=> handleReset()}>Reset</button>
+          <button
+            className="button"
+            onClick={() => {
+              setRunning(!running);
+              if (!running) {
+                runningRef.current = true;
+              }
+              setInterval(() => {
+                runSimulation(grid);
+              }, 500);
+            }}
+          >
+            {running ? "Stop" : "Start"}
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Simulator.jsx
+++ src/components/Simulator.jsx
@@ -1,102 +1,24 @@
-import React, { useCallback, useRef, useState } from "react";
+import React from "react";
 
-let numRows = 20,
-  numCols = 20;
-const genGrid = () => {
-  let rows = [];
-  for (let i = 0; i < numRows; i++) {
-    // rows.push(Array.from(Array(numCols), () => 0));
-    rows.push(Array.from(Array(numCols), () => (Math.floor(Math.random()*2))));
-  }
-  return rows;
-};
-const positions = [
-  [0, 1],
-  [0, -1],
-  [1, -1],
-  [-1, 1],
-  [1, 1],
-  [-1, -1],
-  [1, 0],
-  [-1, 0],
-];
-
-const Simulator = () => {
-  const [grid, setGrid] = useState(() => genGrid());
-  const [running, setRunning] = useState(false);
-  //   console.log(grid);
-
+const Simulator = ({rows, cols, grid, setGrid}) => {
   const handleToggle = (row, col) => {
     let copy = [...grid];
     copy[row][col] = grid[row][col] === 1 ? 0 : 1;
     setGrid(copy);
   };
-
-  // ...
-  const runningRef = useRef(running);
-  runningRef.current = running;
-
-  const runSimulation = useCallback((grid) => {
-    if (!runningRef.current) {
-      return;
-    }
-
-    let gridCopy = [...grid];
-    for (let i = 0; i < numRows; i++) {
-      for (let j = 0; j < numCols; j++) {
-        let neighbors = 0;
-
-        positions.forEach(([x, y]) => {
-          const newI = i + x;
-          const newJ = j + y;
-
-          if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
-            neighbors += grid[newI][newJ];
-          }
-        });
-
-        if (neighbors < 2 || neighbors > 3) {
-          gridCopy[i][j] = 0;
-        } else if (grid[i][j] === 0 && neighbors === 3) {
-          gridCopy[i][j] = 1;
-        }
-      }
-    }
-
-    setGrid(gridCopy);
-  }, []);
   return (
-    <div className="simulator">
-      <div className="grid">
-        {grid.map((row, i) =>
-          row.map((col, j) => (
-            <div
-              className={`cell ${grid[i][j] === 1 ? "alive" : ""}`}
-              key={`${i}-${j}`}
-              onClick={() => handleToggle(i, j)}
-            >
-              {/* {grid[i][j]} */}
-            </div>
-          ))
-        )}
-      </div>
-      <div className="buttons">
-        <button className="button">Reset</button>
-        <button
-          className="button"
-          onClick={() => {
-            setRunning(!running);
-            if (!running) {
-              runningRef.current = true;
-            }
-            setInterval(() => {
-              runSimulation(grid);
-            }, 500);
-          }}
-        >
-          {running ? "Stop" : "Start"}
-        </button>
-      </div>
+    <div className="grid">
+      {grid.map((row, i) =>
+        row.map((col, j) => (
+          <div
+            className={`cell ${grid[i][j] === 1 ? "alive" : ""}`}
+            key={`${i}-${j}`}
+            onClick={() => handleToggle(i, j)}
+          >
+            {/* {grid[i][j]} */}
+          </div>
+        ))
+      )}
     </div>
   );
 };
