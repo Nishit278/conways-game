38ae144d8840ca6353da7cffaff8d693d8df2c14
Nishit Bansal <nishit@Macbook-Air.local>
2023-03-18 10:25:55 +0530

    Initial Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <h1>Conway's Game</h1>
+        <div className="buttons">
+            <button className="button">Rules</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
